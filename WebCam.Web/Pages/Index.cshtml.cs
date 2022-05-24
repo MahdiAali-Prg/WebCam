8a333e35f6188462670f46d017bb8d3843099290
Mahdi Aali <mahdi1383harold@gmail.com>
2022-05-24 18:43:07 +0430

    Add project files.

---
+++ WebCam.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.IO;
+
+namespace WebCam.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+
+        public IActionResult OnPost([FromForm] byte[] image)
+        {
+            if (image != null)
+            {
+                if (! Directory.Exists(Path.Combine(Directory.GetCurrentDirectory(), "WebCamImages")))
+                {
+                    Directory.CreateDirectory("WebCamImages");
+                }
+                string fileName = Guid.NewGuid().ToString();
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "WebCamImages", $"{fileName}.png");
+                using (FileStream file = System.IO.File.Create(path))
+                {
+                    file.Write(image);
+                }
+                return RedirectToPage();
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+        }
+    }
+}
